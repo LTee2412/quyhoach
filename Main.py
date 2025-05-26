fbecb58458e79d3f67f950004ad12d9aaa00814d
Bùi Đức Lâm Tùng <150934941+LTee2412@users.noreply.github.com>
2025-05-27 00:23:43 +0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -6,7 +6,6 @@ import matplotlib.pyplot as plt
 import matplotlib
 matplotlib.use('TkAgg')
 
-# Thêm import Mentor_2
 import Mentor_2
 
 def parse_args():
@@ -16,11 +15,8 @@ def parse_args():
     parser.add_argument('--radius', type=float, default=0.3, help='Ty le de tinh ban kinh trong Mentor')
     parser.add_argument('--C', type=int, default=25, help='Dung luong 1 lien ket')
     parser.add_argument('--w', type=int, default=2, help='Trong so luu luong chuan hoa de xet nut backbone của MENTOR')
-    parser.add_argument('--limit_mentor', type=int, default=0,
-                        help='Gioi han cua thuat toan mentor')
-    parser.add_argument('--debug', type=bool, default=True,
-                        help='Che do Debug')
-
+    parser.add_argument('--limit_mentor', type=int, default=0, help='Gioi han cua thuat toan mentor')
+    parser.add_argument('--debug', type=bool, default=True, help='Che do Debug')
     return parser.parse_args()
 
 def main():
@@ -28,16 +24,18 @@ def main():
     # Tạo topology ngẫu nhiên
     ListPosition, TrafficMatrix = InitialTopo.Global_Init_Topo(args.max, args.num_node, args.debug)
     
-    # Thực hiện Mentor 2 ISP
-    backbone, link_path_count, link_cost, link_cost_changed = Mentor_2.Mentor2_ISP(
+    # Thực hiện Mentor 2 ISP với umin=0.8, alpha=0.2
+    backbone, link_path_count, link_cost, link_cost_changed, link_usage = Mentor_2.Mentor2_ISP(
         ListPosition, TrafficMatrix, args.max, args.C, args.w, args.radius, args.limit_mentor,
         umin=0.8, alpha=0.2, debug=args.debug
     )
     
-    Mentor_2.write_result('mentor2_result.txt', backbone, link_path_count, link_cost, link_cost_changed)
+    # Xuất kết quả ra file
+    Mentor_2.write_result('mentor2_result.txt', backbone, link_path_count, link_cost, link_cost_changed, link_usage)
     print("Đã xuất kết quả Mentor 2 ra mentor2_result.txt")
     
-    Mentor_2.plot_backbone(ListPosition, backbone, args.max)
+    # Vẽ topology backbone
+    Mentor_2.plot_backbone(ListPosition, Mentor_2.prim_dijkstra_backbone_links(ListPosition, [n for n in ListPosition if n.get_name() in backbone]), args.max)
     plt.show()
 
 if __name__ == '__main__':
